ecaecc9d9e834e5f7884cbd7a0a77798bd6da34c
Unknown <tmugetti@gmail.com>
2018-08-26 22:07:40 -0300

    v0.2 raw camera movement

---
+++ Matrix/Assets/CameraMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+	[SerializeField] float speed;
+	void Update () {
+		/*Vector3 rot = Vector3.zero;
+		rot += Vector3.right * Input.GetAxis ("Vertical") * Time.deltaTime * speed;
+		rot += Vector3.up * Input.GetAxis ("Horizontal") * Time.deltaTime * speed;
+		transform.Rotate (rot);
+		Debug.Log (rot);*/
+
+		transform.Rotate (Input.acceleration.x, 0, -Input.acceleration.z);
+	}
+}
