49914ac491bb92752ba6f490b315fbf43d89c508
Unknown <ruiztorresmatias@gmail.com>
2018-08-28 16:00:10 -0300

    v0.4 camera movement
    Camera movement for smartphone

--- Matrix/Assets/Scripts/CameraMovement.cs
+++ Matrix/Assets/Scripts/CameraMovement.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour {
 	[SerializeField] float speed;
+
 	void Update () {
-		/*Vector3 rot = Vector3.zero;
+        /*Vector3 rot = Vector3.zero;
 		rot += Vector3.right * Input.GetAxis ("Vertical") * Time.deltaTime * speed;
 		rot += Vector3.up * Input.GetAxis ("Horizontal") * Time.deltaTime * speed;
 		transform.Rotate (rot);
 		Debug.Log (rot);*/
 
-		transform.Rotate (Input.acceleration.x, 0, -Input.acceleration.z);
-	}
+        //transform.Rotate (Input.acceleration.x, 0, -Input.acceleration.z);
+
+
+        /* MOVIMIENTO PARA CELULAR */
+        transform.rotation = Quaternion.Euler(new Vector3(0, transform.rotation.eulerAngles.y, (Input.acceleration.x) * 90.0f)); 
+
+        /* MOVIMIENTO CON GIROSCOPIO PARA VR */
+        
+
+    }
 }
